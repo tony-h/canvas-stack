87a8c922fca9fc3c7457490f35454082d2f5c328
Kjell-Magne Øierud <kjellm@oierud.net>
2014-11-19 11:04:13 +0100

    poc: Add call to modules API

--- poc/app.rb
+++ poc/app.rb
@@ -10,8 +10,13 @@ require 'json'
 
 Dotenv.load
 
+$account_id     = ENV.fetch 'ACCOUNT_ID'
+$api_host       = ENV.fetch 'API_HOST'
+$api_port       = ENV.fetch 'API_PORT'
+$api_client_id  = ENV.fetch 'API_CLIENT_ID'
+$api_client_key = ENV.fetch 'API_CLIENT_KEY'
+
 $last_url = ""
-$account_id = ENV['ACCOUNT_ID']
 
 class URLCapture < Faraday::Middleware
 
@@ -42,7 +47,7 @@ rescue Faraday::Error => e
   raise
 end
 
-$conn = Faraday.new(url: "http://#{ENV['API_HOST']}:#{ENV['API_PORT']}/") do |c|
+$conn = Faraday.new(url: "http://#$api_host:#$api_port/") do |c|
   c.use Faraday::Request::UrlEncoded
   c.use Faraday::Response::Logger
   c.use URLCapture
@@ -57,8 +62,8 @@ get '/' do
                   # session[state] == request[state]
 
   # request authorization grant
-  url = "http://#{ENV['API_HOST']}:#{ENV['API_PORT']}/login/oauth2/auth"
-  url << "?client_id=#{ENV['API_CLIENT_ID']}"
+  url = "http://#$api_host:#$api_port/login/oauth2/auth"
+  url << "?client_id=#$api_client_id"
   url << "&response_type=#{response_type}"
   url << "&redirect_uri=#{redirect_uri}"
   url << "&state=#{state}"
@@ -72,8 +77,8 @@ get '/grant' do
 
   res = log_faraday_ex do
     $conn.post "/login/oauth2/token", {
-                 client_id: ENV['API_CLIENT_ID'],
-                 client_secret: ENV['API_CLIENT_KEY'],
+                 client_id: $api_client_id,
+                 client_secret: $api_client_key,
                  redirect_uri: redirect_uri,
                  code: code,
                }
@@ -88,27 +93,31 @@ end
 get '/courses' do
   @session = session
 
+  $conn.headers['Authorization'] = "Bearer #{session['access_token']}"
+
   log_faraday_ex do
-    res = $conn.get "/api/v1/accounts/#$account_id/courses", {published: true} do |req|
-      req.headers['Authorization'] = "Bearer #{session['access_token']}"
-    end
+    res = $conn.get "/api/v1/accounts/#$account_id/courses", {published: true}
     @courses_url = $last_url
     @courses = JSON.parse res.body
 
     id = @courses.first["id"]
-    res = $conn.get "/api/v1/courses/#{id}", {'include[]' => 'syllabus_body'} do |req|
-      req.headers['Authorization'] = "Bearer #{session['access_token']}"
-    end
+    res = $conn.get "/api/v1/courses/#{id}", {'include[]' => 'syllabus_body'}
     @course_1_url = $last_url
     @course_1 = JSON.parse res.body
 
+    # FIXME /api/v1/courses/:course_id/front_page
+
     res = $conn.get "/api/v1/courses", {
-                     'include' => ['syllabus_body', 'course_progress'],
-                   } do |req|
-      req.headers['Authorization'] = "Bearer #{session['access_token']}"
-    end
+                      'include' => ['syllabus_body', 'course_progress'],
+                    }
     @my_courses_url = $last_url
     @my_courses = JSON.parse res.body
+
+    course_id = @my_courses.first['id']
+    res = $conn.get "/api/v1/courses/#{course_id}/modules", {include: %w(items)}
+    @modules_url = $last_url
+    @modules = JSON.parse res.body
+
   end
 
   haml :courses
